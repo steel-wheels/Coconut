873a40016c0a0fe1ca5510c9f367bdd676fc739d
tomoo <wheel@tcn.zaq.ne.jp>
2024-08-29 20:26:22 +0900

    CoconutData: Update CNReadline class

--- CoconutData/Source/File/CNReadline.swift
+++ CoconutData/Source/File/CNReadline.swift
@@ -9,6 +9,11 @@ import Foundation
 
 public class CNReadline
 {
+        public enum ApplicationType {
+                case terminal
+                case window
+        }
+
 	private var mLine:	String
 	private var mIndex:	String.Index
 
@@ -16,8 +21,8 @@ public class CNReadline
 	public var isEmpty: Bool { get { return mLine.isEmpty }}
 
 	public init() {
-		self.mLine  = ""
-		self.mIndex = mLine.startIndex
+		self.mLine     = ""
+		self.mIndex    = mLine.startIndex
 	}
 
 	public func insert(string str: String) {
@@ -66,4 +71,78 @@ public class CNReadline
 		self.mLine  = ""
 		self.mIndex = mLine.startIndex
 	}
+
+        public func execute(escapeCode ecode: CNEscapeCode, console cons: CNFileConsole, type typ: ApplicationType) -> String? {
+                var result: String? = nil
+
+                /* decode the command */
+                switch ecode {
+                case .string(let str):
+                        self.insert(string: str)
+                        let ins: CNEscapeCode = .insertSpace(str.count)
+                        print(string: ins.encode(), console: cons)
+                        print(string: ecode.encode(), console: cons)
+                case .delete:
+                        if self.delete() {
+                                switch typ {
+                                case .terminal:
+                                        let bs = CNEscapeCode.backspace.encode()
+                                        print(string: bs,  console: cons)
+                                        print(string: " ", console: cons)
+                                        print(string: bs,  console: cons)
+                                default:
+                                        let dcode: CNEscapeCode = .delete
+                                        print(string: dcode.encode(), console: cons)
+                                }
+                        }
+                case .newline:
+                        print(string: ecode.encode(), console: cons)
+                        /* execute the command */
+                        if !self.isEmpty {
+                                result = self.line
+                                self.clear()
+                        }
+                case .cursorForward(let num):
+                        let delta = self.cursorForward(num)
+                        if delta > 0 {
+                                let newcode: CNEscapeCode = .cursorForward(delta)
+                                print(string: newcode.encode(), console: cons)
+                        }
+                case .cursorBackward(let num):
+                        let delta = self.cursorBackward(num)
+                        if delta > 0 {
+                                let newcode: CNEscapeCode = .cursorBackward(delta)
+                                print(string: newcode.encode(), console: cons)
+                        }
+                default:
+                        print(string: ecode.encode(), console: cons)
+                }
+                return result
+        }
+
+        public func execute(console cons: CNFileConsole, type typ: ApplicationType) -> String? {
+                var result: String? = nil
+                switch cons.inputFile.gets() {
+                case .str(let s):
+                        switch CNEscapeCode.decode(string: s) {
+                        case .ok(let codes):
+                                for code in codes {
+                                        if let str = execute(escapeCode: code, console: cons, type: typ){
+                                                result = str
+                                        }
+                                }
+                        case .error(let err):
+                                cons.error(string: "[Error] " + err.toString() + "\n")
+                        }
+                case .endOfFile, .null:
+                        result = "" // can not continue but no result
+                }
+                return result
+        }
+
+        private func print(string str: String, console cons: CNFileConsole){
+                /* I dont know why this interval is required */
+                cons.print(string: str)
+                Thread.sleep(forTimeInterval: 0.001)
+        }
 }
